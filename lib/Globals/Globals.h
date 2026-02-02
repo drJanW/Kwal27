f40d2b7bc754047f9babf5cf669ca0af618e6762
drWilms <janwilms@gmail.com>
2026-02-02 10:41:18 +0100

    SD: accept SDMANAGER version

--- lib/Globals/Globals.h
+++ lib/Globals/Globals.h
@@ -32,7 +32,7 @@
 #else
   #define DEVICE_PREFIX "MARMER-"
 #endif
-#define FIRMWARE_VERSION DEVICE_PREFIX "260202D"
+#define FIRMWARE_VERSION DEVICE_PREFIX "260202E"
 
 // === Compile-time constants (NOT overridable) ===
 #define SECONDS_TICK 1000
--- sdroot/webgui-src/build.ps1
+++ sdroot/webgui-src/build.ps1
@@ -20,7 +20,7 @@ $modules = @(
 
 # Output ONLY to sdroot - NO local copy to prevent accidental editing
 $sdroot_output = "../kwal.js"
-$version = "20260202D"
+$version = "20260202E"
 
 # Header with DO NOT EDIT warning (in case Copilot is being a cunt)
 $header = @"
